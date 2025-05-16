3b3be3d1717f46cebdc3c9b007fea542015a4e72
= <=>
2025-05-15 21:02:38 -0700

    setup project structure

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const rootDir = require('./utils/getRootDir');
+const path = require('path');
+
 const app = express();
+
+app.set('views','views');
+app.set('view engine','ejs');
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(rootDir,'public')));
 
 app.get('/',(req,res) => {
     res.send("Server started at port: 3000");
